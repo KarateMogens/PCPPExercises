6295ce2881961c8575a22ed2a0e26ff66b854d53
cefr <cefr@itu.dk>
2023-09-11 13:40:05 +0200

    Change all files to Gradle project

---
+++ Assignment1/Exercise2/Week02Exercises/app/src/main/java/exercise2/SynchronizedMonitor.java
@@ -0,0 +1,60 @@
+package exercise2;
+
+public class SynchronizedMonitor {
+
+    private int readAcquires         = 0;
+    private int readReleases    = 0;
+    private boolean writer      = false;
+
+
+    public void readLock() {
+
+        synchronized(this) {
+            try {
+                while (writer) {
+                    this.wait();
+                }
+                readAcquires++;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void readUnlock() {
+
+        synchronized(this) {
+            readReleases++;
+            if (readAcquires == readReleases ) 
+                this.notifyAll();
+        }
+    }
+
+    public void writeLock() {
+
+        synchronized(this) {
+            try {
+                while (writer) {
+                    this.wait();
+                }
+                writer = true;
+                while (readAcquires != readReleases) {
+                    this.wait();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            
+        }
+    }
+
+    public void writeUnlock() {
+
+        synchronized(this) {
+            writer = false;
+            this.notifyAll();
+        }
+    }
+    
+}
